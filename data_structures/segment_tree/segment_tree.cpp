db90d05cb206346ed8837f42664ceb47fd3452bc
tiagopio <tiagofonsecapio2018@gmail.com>
2025-10-06 12:26:53 -0300

    new seg

--- data_structures/segment_tree/segment_tree.cpp
+++ data_structures/segment_tree/segment_tree.cpp
@@ -11,39 +11,35 @@ struct Seg {
     inline int left(int p) { return (p << 1); }
     inline int right(int p) { return (left(p) | 1); }
     
-    struct node {
-        long long val;        
-    };
-    
     // neutral element of operation
-    const node INF = {0};
+    const int INF = 0;
     
-    int N;
-    vector<node> segtree;
-    Seg(int n, bool flag = false) : N(n), segtree(n << 2) {
-        if (flag) build(1, 0, N - 1);
+    int n;
+    vector<int> &a, segtree;
+    Seg(int N, vector<int> A, bool flag) : n(N), a(A), segtree(N << 2) {
+        build(1, 0, n - 1);
     }
 
-    node merge(node x, node y) {
-        return { x.val + y.val };
+    int merge(int x, int y) {
+        return x + y;
     }
 
-    node build(int p, int l, int r) {
-        if (l == r) return segtree[p] = { a[l] };
+    int build(int p, int l, int r) {
+        if (l == r) return segtree[p] = a[l];
         int m = (l + r) >> 1;
         return segtree[p] = merge(build(left(p), l, m), build(right(p), m + 1, r));
     }
 
-    int query(int l, int r) { return query(l, r, 1, 0, N - 1).val; }
-    node query(int a, int b, int p, int l, int r) {
+    int query(int a, int b) { return query(a, b, 1, 0, n - 1); }
+    int query(int a, int b, int p, int l, int r) {
         if (b < l or r < a) return INF;
         if (a <= l and r <= b) return segtree[p];
         int m = (l + r) >> 1;
         return merge(query(a, b, left(p), l, m), query(a, b, right(p), m + 1, r));
     }
 
-    node update(int i, int x) { return update(i, x, 1, 0, N - 1); }
-    node update(int i, int x, int p, int l, int r) {
+    int update(int i, int x) { return update(i, x, 1, 0, n - 1); }
+    int update(int i, int x, int p, int l, int r) {
         if (i < l or r < i) return segtree[p];
         if (l == r) return segtree[p] = { x };
         int m = (l + r) >> 1;
